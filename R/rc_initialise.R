c485a17f2f8224a981d5b9c555e5ab8cd2034b77
FrankvRaffe <frank.vanraffe@nmi-agro.nl>
2025-08-28 17:23:54 +0200

    Updated rc_shi_field and rc_initialize with new soil input

--- R/rc_initialise.R
+++ R/rc_initialise.R
@@ -1,16 +1,15 @@
 #' Function for initializing the RothC model
 #'
 #' @param B_LU_BRP (numeric) value of the BRP crop code
-#' @param A_SOM_LOI (numeric) value for the soil organic matter content of the soil
-#' @param A_CLAY_MI (numeric) value for the clay content of the soil
+#' @param soil_properties (list) List with soil properties: A_C_OF, soil organic carbon content (g/kg) or B_C_ST03, soil organic carbon stock (Mg C/ha), preferably for soil depth 0.3 m; A_CLAY_MI, clay content (\%); A_DENSITY_SA, dry soil bulk density (g/cm3)
 #'
 #' @import data.table
 #'
 #' @export
-rc_initialise <- function(B_LU_BRP,A_SOM_LOI,A_CLAY_MI){
+rc_initialise <- function(B_LU_BRP,soil_properties){
   
   # add visual bindings
-  . = CIOM = CDPM = CRPM = CBIO = NULL
+  . = CIOM = CDPM = CRPM = CBIO = A_SOM_LOI = NULL
   
   # Prepare input for scenario Business As Usual
   scen.inp <- rc_input_scenario(B_LU_BRP = B_LU_BRP, scen = 'BAU')
@@ -21,8 +20,7 @@ rc_initialise <- function(B_LU_BRP,A_SOM_LOI,A_CLAY_MI){
   parms <- list(simyears = 150,unit = 'psomperfraction', initialize = TRUE)
   
   # Run initialization run for 150 years
-  this.result <- rc_sim(A_SOM_LOI = A_SOM_LOI,
-                               A_CLAY_MI = A_CLAY_MI,
+  this.result <- rc_sim(soil_properties = soil_properties,
                                rothc_rotation = rotation,
                                rothc_amendment = amendment,
                                rothc_parms = parms)
