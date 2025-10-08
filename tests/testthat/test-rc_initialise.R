8a24a2a84f33f016dbc750b217a7f21e41f4f7cf
FrankvRaffe <frank.vanraffe@nmi-agro.nl>
2025-10-08 14:01:02 +0200

    Add and updated unit tests

---
+++ tests/testthat/test-rc_initialise.R
@@ -0,0 +1,82 @@
+test_that("rc_initialise runs properly", {
+  # Create valid inputs
+ # Set seed for reproducibility
+  set.seed(123)
+  
+  # Create an event table with random C inputs
+rothc.event <- data.table(
+  time = rep(1:18, times = 3),
+  var = rep(c("CDPM", "CRPM", "CHUM"), each = 18),
+  method = "add",
+  value = sample(500:2000, size = 54, replace = TRUE)
+)
+
+  dt.soc <- data.table(A_CLAY_MI = 18,
+                     A_C_OF = 20,
+                     toc = 210000)
+  
+dt.time <- rc_time_period(start_date = "2022-04-01", end_date = "2040-10-01")
+
+crops <- data.table(
+  B_LU_START = c("2022-04-01", "2023-04-01"),
+  B_LU_END = c("2022-10-01", "2023-10-01"),
+  B_LU = c("nl_308", "nl_308"),
+  B_LU_NAME = c("erwten (droog te oogsten)", "erwten (droog te oogsten)" ),
+  B_LU_HC = c(0.32, 0.32),
+  B_C_OF_INPUT = c(1500, 1500)
+)
+
+amendment <- data.table(
+  P_ID = c(1, 1),
+  P_NAME = c('cattle_slurry', 'cattle_slurry'),
+  P_DOSE = c(63300, 63300),
+  P_HC = c(0.7,0.7),
+  P_C_OF = c(35, 35),
+  P_DATE_FERTILIZATION = c("2022-05-01", "2023-05-01"))
+
+
+# Calculate rothc.parms
+dt.weather <- data.table(month = 1:12,
+                         W_TEMP_MEAN_MONTH = c(3.6,3.9,6.5,9.8,13.4,16.2,18.3,17.9,14.7,10.9,7,4.2),
+                         W_PREC_SUM_MONTH = c(70.8, 63.1, 57.8, 41.6, 59.3, 70.5, 85.2, 83.6, 77.9, 81.1, 80.0, 83.8),
+                         W_ET_POT_MONTH = c(8.5, 15.5, 35.3, 62.4, 87.3, 93.3, 98.3, 82.7, 51.7, 28.0, 11.3,  6.5),
+                         W_ET_ACT_MONTH = NA_real_)
+
+dt.rmf <- rc_input_rmf(dt = crops,
+                       B_DEPTH = 0.3,
+                       A_CLAY_MI = dt.soc$A_CLAY_MI,
+                       dt.weather = dt.weather,
+                       dt.time = dt.time
+                       )
+
+rothc.parms <- list(k1 = 10,
+                    k2 = 0.3,
+                    k3=0.66,
+                    k4=0.02,
+                    R1 = dt.rmf$R1,
+                    abc = dt.rmf$abc,
+                    time = dt.rmf$time)
+
+
+ 
+
+
+# Check with all valid inputs
+# spinup_analytical_bodemcoolstof
+expect_no_error(rc_initialise(crops = crops,
+                              amendment = amendment,
+                              dt.soc = dt.soc,
+                              rothc.parms = rothc.parms,
+                              rothc.event = rothc.event,
+                              dt.time = dt.time,
+                              type = 'spinup_analytical_bodemcoolstof'))
+
+# spinup_analytical_heuvelink
+rc_initialise(crops = crops,
+                              amendment = amendment,
+                              dt.soc = dt.soc,
+                              rothc.parms = rothc.parms,
+                              rothc.event = rothc.event,
+                              dt.time = dt.time,
+                              type = 'spinup_analytical_heuvelink')
+})
--- tests/testthat/test-rc_sim.R
+++ tests/testthat/test-rc_sim.R
@@ -44,7 +44,7 @@ test_that("rc_sim correctly checks input validity", {
   
   parms <- list(dec_rates = c(k1 = 10, k2 = 0.3, k3 = 0.66, k4 = 0.02),
                       c_fractions = c(fr_IOM = 0.049, fr_DPM = 0.015, fr_RPM = 0.125, fr_BIO = 0.015),
-                      initialize = TRUE,
+                      initialize = 'spinup_analytical_bodemcoolstof',
                       unit = "A_SOM_LOI",
                       method = "adams",
                       poutput = "year",
@@ -52,10 +52,10 @@ test_that("rc_sim correctly checks input validity", {
                       end_date = "2040-10-01")
 
   # All correct
- expect_no_error(rc_sim(soil_properties = soil_properties, A_DEPTH = A_DEPTH,
+rc_sim(soil_properties = soil_properties, A_DEPTH = A_DEPTH,
                    B_DEPTH = B_DEPTH, cf_yield = cf_yield, M_TILLAGE_SYSTEM = M_TILLAGE_SYSTEM,
                    rothc_rotation = rothc_rotation, rothc_amendment = rothc_amendment, 
-                   weather = weather, rothc_parms = parms))
+                   weather = weather, rothc_parms = parms)
   
   # No amendment table (allowed)
   expect_no_error(rc_sim(soil_properties = soil_properties, A_DEPTH = A_DEPTH,
