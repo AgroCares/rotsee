f2d515ffe1aa211c5c87bdc8cc66ac206dec2a97
FrankvRaffe <frank.vanraffe@nmi-agro.nl>
2025-10-20 12:25:24 +0200

    Fixed error in c_fractions when initialize was false

--- R/rc_sim.R
+++ R/rc_sim.R
@@ -96,8 +96,11 @@ rc_sim <- function(soil_properties,
   k3 <- rothc_parms$dec_rates[["k3"]]
   k4 <- rothc_parms$dec_rates[["k4"]]
   
-  # Define C fractions
-  c_fractions <- rothc_parms$c_fractions
+  # Define C fractions  
+  fr_IOM <- rothc_parms$c_fractions[["fr_IOM"]]
+  fr_DPM <- rothc_parms$c_fractions[["fr_DPM"]]
+  fr_RPM <- rothc_parms$c_fractions[["fr_RPM"]]
+  fr_BIO <- rothc_parms$c_fractions[["fr_BIO"]]
   
   # Define unit of output
   unit <- rothc_parms$unit
@@ -176,7 +179,7 @@ rc_sim <- function(soil_properties,
   }else{
     dt.soc[,toc := A_C_OF / 1000 * A_DENSITY_SA * 1000 * B_DEPTH * 100 * 100]
   }
-  
+
   # set the default initialisation to the one used in BodemCoolstof
   if(initialize == TRUE){
     
@@ -225,10 +228,10 @@ rc_sim <- function(soil_properties,
   } else {
     
     # Calculate carbon pools based on provided or default distribution (kg C / ha)
-    dt.soc[,CIOM0 := c_fractions$fr_IOM * ((toc*0.001)^1.139) * 1000]
-    dt.soc[,CDPM0 := c_fractions$fr_DPM * (toc-CIOM0)]
-    dt.soc[,CRPM0 := c_fractions$fr_RPM * (toc-CIOM0)]
-    dt.soc[,CBIO0 := c_fractions$fr_BIO * (toc-CIOM0)]
+    dt.soc[,CIOM0 := fr_IOM * ((toc*0.001)^1.139) * 1000]
+    dt.soc[,CDPM0 := fr_DPM * (toc-CIOM0)]
+    dt.soc[,CRPM0 := fr_RPM * (toc-CIOM0)]
+    dt.soc[,CBIO0 := fr_BIO * (toc-CIOM0)]
     dt.soc[,CHUM0 := toc-CIOM0-CDPM0-CRPM0-CBIO0]
     
   }
