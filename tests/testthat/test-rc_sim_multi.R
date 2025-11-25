94e4e25b7487d083339abae6f12c142ab565a4eb
FrankvRaffe <frank.vanraffe@nmi-agro.nl>
2025-11-25 11:13:05 +0100

    update multicore tests

--- tests/testthat/test-rc_sim_multi.R
+++ tests/testthat/test-rc_sim_multi.R
@@ -17,16 +17,12 @@ test_that("rc_sim_multi runs with normal inputs", {
     ID = rep(c('high', 'low', 'mid'),2),
     B_LU_START = rep(c("2022-04-01", "2023-04-01"),each=3),
     B_LU_END = rep(c("2022-10-01", "2023-10-01"),each=3),
-    B_LU = rep(c("nl_308", "nl_308"),each = 3),
-    B_LU_NAME = rep(c("erwten (droog te oogsten)", "erwten (droog te oogsten)" ),each =3),
     B_LU_HC = rep(0.32, 6),
     B_C_OF_INPUT = rep(1500, 6)
   )
   
   rothc_amendment <- data.table(
     ID = rep(c('high', 'low', 'mid'),2),
-    P_ID = rep(1, 6),
-    P_NAME = rep('cattle_slurry', 6),
     P_DOSE = rep(c(100000, 25000, 70000), 2),
     P_HC = rep(0.7,6),
     P_C_OF = rep(35, 6),
@@ -100,16 +96,12 @@ test_that("rc_sim_multi handles progress reporting", {
     ID = rep(c('high', 'low', 'mid'),2),
     B_LU_START = rep(c("2022-04-01", "2023-04-01"),each=3),
     B_LU_END = rep(c("2022-10-01", "2023-10-01"),each=3),
-    B_LU = rep(c("nl_308", "nl_308"),each = 3),
-    B_LU_NAME = rep(c("erwten (droog te oogsten)", "erwten (droog te oogsten)" ),each=3),
     B_LU_HC = rep(0.32, 6),
     B_C_OF_INPUT = rep(1500, 6)
   )
   
   rothc_amendment <- data.table(
     ID = rep(c('high', 'low', 'mid'),2),
-    P_ID = rep(1, 6),
-    P_NAME = rep('cattle_slurry', 6),
     P_DOSE = rep(c(100000, 25000, 70000), 2),
     P_HC = rep(0.7,6),
     P_C_OF = rep(35, 6),
@@ -119,13 +111,12 @@ test_that("rc_sim_multi handles progress reporting", {
   weather <- data.table(month = 1:12,
                         W_TEMP_MEAN_MONTH = c(3.6,3.9,6.5,9.8,13.4,16.2,18.3,17.9,14.7,10.9,7,4.2),
                         W_PREC_SUM_MONTH = c(70.8, 63.1, 57.8, 41.6, 59.3, 70.5, 85.2, 83.6, 77.9, 81.1, 80.0, 83.8),
-                        W_ET_POT_MONTH = c(8.5, 15.5, 35.3, 62.4, 87.3, 93.3, 98.3, 82.7, 51.7, 28.0, 11.3,  6.5),
+                        W_ET_REF_MONTH = c(8.5, 15.5, 35.3, 62.4, 87.3, 93.3, 98.3, 82.7, 51.7, 28.0, 11.3,  6.5),
                         W_ET_ACT_MONTH = NA_real_)
   
   parms <- list(dec_rates = c(k1 = 10, k2 = 0.3, k3 = 0.66, k4 = 0.02),
                 c_fractions = c(fr_IOM = 0.049, fr_DPM = 0.015, fr_RPM = 0.125, fr_BIO = 0.015),
                 initialize = TRUE,
-                simyears = 50,
                 unit = "A_SOM_LOI",
                 method = "adams",
                 poutput = "year",
@@ -144,6 +135,7 @@ test_that("rc_sim_multi handles progress reporting", {
                          strategy = 'multisession')
   
   expect_s3_class(result, "data.table")
+  expect_equal(nrow(result), 57)
 })
 
 
