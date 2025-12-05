9377aa447a1295e9c7839db266a88800116b39c4
FrankvRaffe <frank.vanraffe@nmi-agro.nl>
2025-12-05 16:18:29 +0100

    Added explanation on fraction check

--- R/rc_helpers.R
+++ R/rc_helpers.R
@@ -233,6 +233,9 @@ rc_update_parms <- function(parms = NULL, crops = NULL, amendments = NULL){
     if('fr_BIO' %in% rcp){fr_BIO <- parms$c_fractions[['fr_BIO']]}
     
     # check supplied fractions do not exceed 1
+    # Note: fr_IOM is excluded from sum check as it's a coefficient in the power-law
+    # formula (CIOM = fr_IOM * toc^1.139), while other fractions partition the
+    # reactive C pool (toc - CIOM)
     if ((fr_DPM + fr_RPM + fr_BIO) > 1 + 1e-10) {
       stop("Sum of c_fractions (fr_DPM + fr_RPM + fr_BIO) exceeds 1; please reduce one or more fractions.")
       }
